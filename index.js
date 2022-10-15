1c0531dd3bbccbe39caba3b3b359abb6018a708d
Seeta Genova <seetagenova@yahoo.com>
2022-10-15 13:11:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Titanic',
+  },
+  {
+    title: 'The Lion King'
+  }
+];
+
+app.get('/movies', (req, res)=> {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix movie app!');
+})
