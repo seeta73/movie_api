890763e89cc9422a2697ff2badc794eb0873f0c7
Seeta Genova <seetagenova@yahoo.com>
2022-10-27 19:57:50 -0400

    update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/documentation',(req,res) => {
 
 // CREATE, POST method - Allow new users to register
 // tested in Postman?
-app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users', (req, res) => {
   Users.findOne({ Username: req.body.Username })
   .then((user) => {
     if (user) {
